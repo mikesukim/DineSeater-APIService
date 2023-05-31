f86181e2e5a007e970c7c56e2e5de5782140b14a
mikesukim <mikesungunkim@gmail.com>
2023-05-31 00:10:38 -0700

    add a logic of retrieving business_name from id_token

--- Post-AddToWaitingList/lambda_function.py
+++ Post-AddToWaitingList/lambda_function.py
@@ -1,9 +1,24 @@
-'''
-Entry point of lambda function's execution.
-This lambda function will be executed at pre-token-generation phase as Cognito trigger.
-'''
-
 import json
 
 def lambda_handler(event, context):
-    return event
+    business_name = get_claim(event, 'business_name')  
+    if not business_name or business_name == None:
+        return {
+            'statusCode': 400,
+            'body': 'Business name not found'
+        } 
+    
+    print("business name: " + business_name)
+    return {
+        'statusCode': 200,
+        'body': 'Claim retrieved successfully'
+    }
+
+
+def get_claim(event, claim_name):
+    authorizer = event['requestContext'].get('authorizer')
+    if authorizer and 'claims' in authorizer:
+        claims = authorizer['claims']
+        if claim_name in claims:
+            return claims[claim_name]
+    return None
