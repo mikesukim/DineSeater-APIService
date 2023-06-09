85d827d6c35d91df7adb73992a32443e5ea31dc2
mikesukim <mikesungunkim@gmail.com>
2023-06-08 23:14:43 -0700

    add get logic of retrieving all business waitings

--- Waitinglist/get_handler.py
+++ Waitinglist/get_handler.py
@@ -1,5 +1,6 @@
 import json
 import response_handler
+import decimal
 
 class GetHandler:
     def __init__(self, event, business_name, dynamodb_client):
@@ -10,8 +11,20 @@ class GetHandler:
     def handle_action(self):
         try:
             # get waitinglist from dynamodb
-            return response_handler.success({"message": "Hello " + self.business_name})
+            waitings = self.dynamodb_client.get_waitings_by_business_name(self.business_name)
+            print('Successfully get waitings: ' + str(waitings))
+            response_body = {
+                "message": "Successfully get waitinglists",
+                "waitings": waitings
+            }
+            # dynamodb returns number_of_customers as a Decimal type which is not JSON serializable. Convert it to int.
+            def decimal_default(obj):
+                if isinstance(obj, decimal.Decimal):
+                    return int(obj)
+                raise TypeError
+            
+            return response_handler.success(json.dumps(response_body, default=decimal_default))
         except Exception as e:
-            error_message = 'Error handling get request: ' + str(e)
+            error_message = 'Error while handling get request: ' + str(e)
             print(error_message)
             return response_handler.bad_request({"message": error_message})
