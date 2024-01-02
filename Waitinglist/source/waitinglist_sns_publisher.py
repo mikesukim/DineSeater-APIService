9731f2c69fd9eb91311fd7e2f76c4fadf004446c
mikesukim <mikesungunkim@gmail.com>
2024-01-02 00:13:39 -0800

    fix: update FCM message format

--- Waitinglist/source/waitinglist_sns_publisher.py
+++ Waitinglist/source/waitinglist_sns_publisher.py
@@ -37,7 +37,7 @@ class WaitinglistSNSPublisher:
         # There should be only one backslash at a time in the message. Having consecutive backslashes will not publish the message, without any error message.
         message = {
             "default": "Sample fallback message",
-            "GCM": "{ \"notification\": { \"title\": \"" + title + "\", \"body\": \"" + body + "\"}, \"data\": " + json.dumps(data, ensure_ascii=False)  + "}",
+            "GCM": "{ \"notification\": { \"title\": \"" + title + "\", \"body\": \"" + body + "\"}, \"data\": { \"waiting\" :" + json.dumps(data, ensure_ascii=False)  + "}}",
         }
         return json.dumps(message)
     
